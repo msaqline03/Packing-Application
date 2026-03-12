89961e12026209706e76f564c0c9a8d6b50dfe87
afaiyaz04 <ahbabfaiyaz@gmail.com>
2026-03-12 17:01:08 +1100

    Added .env in ignore for whole application

--- Mahonys-Packing-frontend/.next/server/middleware-build-manifest.js
+++ Mahonys-Packing-frontend/.next/server/middleware-build-manifest.js
@@ -24,6 +24,11 @@ self.__BUILD_MANIFEST = {
       "static/chunks/main.js",
       "static/chunks/pages/customers.js"
     ],
+    "/userPermissions": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/userPermissions.js"
+    ],
     "/users": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
--- Mahonys-Packing-frontend/.next/static/chunks/webpack.js
+++ Mahonys-Packing-frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b8c17f05b8ff80a5"; }
+/******/ 		__webpack_require__.h = function() { return "b54c0feabb3498c9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
